d334fc580e31dd42a7ab562cacc8e59a900b6589
20athira <athiraramachanadran235@gmail.com>
2024-02-13 22:35:50 +0530

    Security app frontend

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(home());
+}
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
